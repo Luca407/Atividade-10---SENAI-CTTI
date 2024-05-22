e53af30cfb047cbe10ebeb6a9d224d9a5f15244e
Luca407 <0001081825@senaimgaluno.com.br>
2024-05-22 10:45:19 -0300

    02.C

---
+++ 02.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+float nota1;
+float nota2;
+float nota3;
+float nota4;
+
+
+	printf("Digite a primeira nota:\t");
+	scanf("%f", &nota1);
+	printf("Digite a segunda nota:\t");
+	scanf("%f", &nota2);
+	printf("Digite a terceira nota:\t");
+	scanf("%f", &nota3);
+	printf("Digite a quarta nota:\t");
+	scanf("%f", &nota4);
+	
+float media = nota1+nota2+nota3+nota4/4;
+
+printf("Nota media: %f", media);
+
+return 0;
+}
